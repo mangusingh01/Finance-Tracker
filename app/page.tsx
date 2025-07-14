a41aa01aba97e670a42dd5132bdcb6d6efb48812
Mangu Singh <mangusingh@Mangus-MacBook-Pro.local>
2025-07-14 15:43:38 +0530

    DB connection

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import {
-  featuresData,
-  howItWorksData,
-  statsData,
-  testimonialsData,
-} from "@/data/landing";
-import Image from "next/image";
+import { featuresData, howItWorksData, statsData } from "@/data/landing";
 import Link from "next/link";
 
 export default function Home() {
