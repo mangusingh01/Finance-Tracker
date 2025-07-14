4b00a7f3c9c0b4836d9402b5ab1bd82e3dd8610c
Mangu Singh <mangusingh@Mangus-MacBook-Pro.local>
2025-07-14 18:43:43 +0530

    checking ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
           {/* {footer} */}
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-grey-600">
-              Made by Mangu Singh......
+              Made for finanaces, made for learning......
             </div>
           </footer>
         </body>
